83cef06088fe091e25f357a8341b909b09f99064
Alba Castano <acastano@student.hive.fi>
2022-06-09 16:33:46 +0300

    removed unnecessary lines (trimming twice)

--- ex00/another_world.php
+++ ex00/another_world.php
@@ -3,9 +3,6 @@
         if($argc > 1)
         {
             $string = trim(preg_replace('/\s+/', ' ', $argv[1]));
-            $array=explode(' ',$string);
-            $array=array_filter($array);
-            $string=implode(' ',$array);
             echo $string."\n";
         }
 ?>
